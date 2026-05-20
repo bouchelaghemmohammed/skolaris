84b3cc5853200296dbdc728a4d61b2c4b6badf33
Aicha MT <1770667@cgodin.qc.ca>
2026-05-19 21:31:22 -0400

    Add IsPublie property to EmploiDuTemps class
    Added IsPublie property to track publication status.

--- Models/EmploiDuTemps.cs
+++ Models/EmploiDuTemps.cs
@@ -4,12 +4,13 @@ namespace Skolaris.Models
 {
     public class EmploiDuTemps
     {
-        public int IdEmploi { get; set; }
-        public JourSemaine JourSemaine { get; set; }
-        public TimeSpan HeureDebut { get; set; }
-        public TimeSpan HeureFin { get; set; }
-        public string? Salle { get; set; }
-        public int IdCoursOffert { get; set; }
+        public int        IdEmploi      { get; set; }
+        public JourSemaine JourSemaine  { get; set; }
+        public TimeSpan   HeureDebut    { get; set; }
+        public TimeSpan   HeureFin      { get; set; }
+        public string?    Salle         { get; set; }
+        public int        IdCoursOffert { get; set; }
+        public bool       IsPublie      { get; set; } = false;
 
         public CoursOffert CoursOffert { get; set; } = null!;
     }
