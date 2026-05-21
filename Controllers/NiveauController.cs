d7f67c1818c598c494e7ef83c39812ddb929269c
jeffsawma <jeff.sawma@live.com>
2026-05-20 22:35:27 -0400

    Added CRUD operations for niveaux

--- Controllers/NiveauController.cs
+++ Controllers/NiveauController.cs
@@ -1,5 +1,8 @@
+using Skolaris.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using Skolaris.Data;
+using Skolaris.Models;
 
 namespace Skolaris.Controllers
 {
@@ -25,6 +28,21 @@ namespace Skolaris.Controllers
             return Ok(niveaux);
         }
 
+        // GET: api/Niveau/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var niveau = _context.Niveaux
+                .Where(n => n.IdNiveau == id)
+                .Select(n => new { n.IdNiveau, n.Nom, n.IdProgramme })
+                .FirstOrDefault();
+
+            if (niveau == null)
+                return NotFound();
+
+            return Ok(niveau);
+        }
+
         // GET: api/Niveau/programme/{idProgramme}
         [HttpGet("programme/{idProgramme}")]
         public IActionResult GetByProgramme(int idProgramme)
@@ -35,6 +53,59 @@ namespace Skolaris.Controllers
                 .OrderBy(n => n.Nom)
                 .ToList();
             return Ok(niveaux);
+        }
+
+        // POST: api/Niveau
+        [HttpPost]
+        public IActionResult Create(Niveau niveau)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Niveaux.Add(niveau);
+            _context.SaveChanges();
+
+            return Ok(niveau);
+        }
+
+
+        // PUT: api/Niveau/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Niveau updatedNiveau)
+        {
+            if (id != updatedNiveau.IdNiveau)
+                return BadRequest();
+
+            var niveau = _context.Niveaux.Find(id);
+
+            if (niveau == null)
+                return NotFound();
+
+            niveau.Nom = updatedNiveau.Nom;
+            niveau.IdProgramme = updatedNiveau.IdProgramme;
+
+            _context.SaveChanges();
+
+            return Ok(niveau);
+        }
+
+        // Delete: api/Niveau/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            var niveau = _context.Niveaux.Find(id);
+
+            if (niveau == null)
+            {
+                return NotFound();
+            }
+
+            _context.Niveaux.Remove(niveau);
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
